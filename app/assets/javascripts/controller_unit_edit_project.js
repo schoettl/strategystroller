cc079d44c5e5378d05165c1c6f5e46127bed65fd
Jakob Schöttl <jschoett@gmail.com>
2015-01-10 14:17:41 +0100

    Removed filedrop cause not sure if it work in IE8

--- app/assets/javascripts/controller_unit_edit_project.js
+++ app/assets/javascripts/controller_unit_edit_project.js
@@ -20,41 +20,6 @@ $(function() {
     $('#project_is_project').click(function() {
         enableProjectTabs(this.checked);
     });
-
-/*
-    $('#document_table tbody').on('dblclick', 'tr', function() {
-        alert('download file: ' + $(this).text());
-    });
-
-    var uploadUrl = '../documents';
-    var options = {iframe: {url: uploadUrl}}; // relativ -> controller_unit/project/:project_id/documents
-    var zone = new FileDrop('document-drop-zone', options);
-
-    zone.event('send', function(files) {
-        files.each(function(file) {
-            file.event('done', function(xhr) {
-                alert('Done uploading ' + this.name + ',' +
-                ' response:\n\n' + xhr.responseText);
-            });
-
-            file.event('error', function(e, xhr) {
-                alert('Error uploading ' + this.name + ': ' +
-                xhr.status + ', ' + xhr.statusText);
-            });
-
-            file.sendTo(uploadUrl);
-        });
-    });
-
-    // <iframe> uploads are special - handle them.
-    zone.event('iframeDone', function(xhr) {
-        alert('Done uploading via <iframe>, response:\n\n' + xhr.responseText);
-    });
-
-    // Toggle multiple file selection in the File Open dialog.
-    fd.addEvent(fd.byID('zbasicm'), 'change', function(e) {
-        zone.multiple((e.currentTarget || e.srcElement).checked);
-    }); */
 });
 
 function enableAppropriateTabs() {
