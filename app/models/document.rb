1c0e4c72e95ae44c1b0f2aa4d08a235281028a07
Jakob Schöttl <jschoett@gmail.com>
2015-01-11 22:17:56 +0100

    Bugfixes (syntax?) and file size constraint for document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,11 +1,15 @@
 class Document
 
+  MAX_FILE_SIZE = 20e6
+
   PROJECT_DOCUMENT_FOLDER = 'project_documents'
   PUBLIC_PATH = '/public'
 
   def self.save(upload, project_id)
     filename = File.basename upload.original_filename
-    raise if %w(. ..).include? filename
+    if %w(. ..).include?(filename) || upload.size > MAX_FILE_SIZE
+      raise 'Invalid parameters: original filename or size'
+    end
     path = File.join(document_folder_path(project_id), filename)
     FileUtils.mkdir_p File.dirname(path)
     File.open(path, 'wb') { |f| f.write(upload.read) }
@@ -21,7 +25,9 @@ class Document
 
   def self.document_folder_path(project_id)
     folder = project_id.to_s
-    raise unless folder.match /\d+/
+    unless folder.match /\d+/
+      raise 'Invalid project id'
+    end
     File.join(Rails.root, PUBLIC_PATH, PROJECT_DOCUMENT_FOLDER, folder)
   end
 
